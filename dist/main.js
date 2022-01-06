fe5c9f3d9e6fcf6f5f53fbf8f1bf3f1724c62964
nekokamu <spaletcano@gmail.com>
2022-01-06 13:51:54 -0300

    Se añadieron variables con los diferentes tipos de datos de TypeScript

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,25 @@
 "use strict";
 console.log("Main");
+// Number
+let numero; // Explicito
+numero = 5;
+let inferido = 10; // Inferido
+let color = 0x00FFC0;
+let binario = 0b1010;
+let octal = 0o744;
+// Boolean
+let booleano = true;
+let isVisited = false;
+// String
+let stringcito = "Hola";
+let ejemplito = "UwU";
+// Template String
+// Uso de back-tick `
+let userInfo;
+userInfo = `
+    User Info:
+    username: ${ejemplito}
+    isVisited: ${isVisited}
+    Favorite Color: ${color}
+`;
+console.log('userInfo', userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 console.log("Main");
+
+// Number
+let numero: number; // Explicito
+numero = 5;
+let inferido = 10; // Inferido
+
+let color: number = 0x00FFC0
+let binario: number = 0b1010;
+let octal: number = 0o744;
+
+// Boolean
+let booleano: boolean = true;
+let isVisited = false;
+
+// String
+let stringcito: string = "Hola";
+let ejemplito = "UwU";
+
+// Template String
+// Uso de back-tick `
+let userInfo: string;
+userInfo = `
+    User Info:
+    username: ${ejemplito}
+    isVisited: ${isVisited}
+    Favorite Color: ${color}
+`;
+
+console.log('userInfo', userInfo);
